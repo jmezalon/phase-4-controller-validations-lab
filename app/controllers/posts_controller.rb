ca37614ccc75cba6484a17c515758b57a8d8093f
Max Mezalon <jmezalon@gmail.com>
2021-12-14 10:12:28 -0500

    commit tree 53aef4a851c1033b280314078e1cd1e55c640859

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record 
 
   def show
     post = Post.find(params[:id])
@@ -9,9 +10,11 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
+  # rescue ActiveRecord::RecordInvalid => invalid 
+  #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
@@ -20,4 +23,7 @@ class PostsController < ApplicationController
     params.permit(:category, :content, :title)
   end
 
+  def render_invalid_record(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 end
